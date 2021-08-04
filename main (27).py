35cfdd7a1e1f3fa4e330d71dfe83c110267fec1c
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-04 16:43:46 -0400

    Add files via upload

---
+++ main (27).py
@@ -0,0 +1,9 @@
+color = input("Enter a Color: ")
+pluralNoun = input("Enter a Plural Noun: ")
+person = input("Enter the name of a celebrity: ")
+book=input("Enter the name of a book: ")
+
+print("Your hair is", color)
+print(pluralNoun, "are garbage")
+print("Children are eaten by", person)
+print(book, "is worse than The Hobbit")
